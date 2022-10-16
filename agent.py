7e0204fa1fb89c2b0401f148c751c2b68fdb2a90
Niranjana Sathish Avilery <niranjana.sathish1997@gmail.com>
2022-10-16 10:22:27 -0400

    agent.py created and modules imported

---
+++ agent.py
@@ -0,0 +1,6 @@
+# PyTorch is an optimized tensor library for deep learning using GPUs and CPUs.
+import torch
+# this library is used to import stack 
+from collections import deque
+# importing the snake_game module
+from snake_game import Nokia_Game, Direction, Point
